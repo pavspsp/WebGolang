cd911dfe79552bceeafe5345bc1ab96ad500c45b
pavspsp <sppavelsp000@gmail.com>
2025-04-26 01:14:58 +0300

    Readall, regexp fixed

--- fastSearch_pprof/fast.go
+++ fastSearch_pprof/fast.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
 	"io"
+	_ "io/ioutil"
+	"os"
+	"regexp"
+	"strings"
 )
 
 // вам надо написать более быструю оптимальную этой функции
@@ -14,5 +21,95 @@ func FastSearch(out io.Writer) {
 		перечитайте еще раз задание
 		!!! !!! !!!
 	*/
-	SlowSearch(out)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	r := regexp.MustCompile("@")
+	androidR := regexp.MustCompile(`Android`)
+	msieR := regexp.MustCompile(`MSIE`)
+	seenBrowsers := []string{}
+	uniqueBrowsers := 0
+	//foundUsers := ""
+	foundusersBuilder := strings.Builder{}
+	userNumber := -1
+	//users := make([]map[string]interface{}, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+		user := make(map[string]interface{})
+		// fmt.Printf("%v %v\n", err, line)
+		err := json.Unmarshal([]byte(line), &user)
+		if err != nil {
+			panic(err)
+		}
+		userNumber++
+		//users = append(users, user)
+
+		isAndroid := false
+		isMSIE := false
+
+		browsers, ok := user["browsers"].([]interface{})
+		if !ok {
+			// log.Println("cant cast browsers")
+			continue
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				// log.Println("cant cast browser to string")
+				continue
+			}
+			if ok := androidR.MatchString(browser); ok {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		for _, browserRaw := range browsers {
+			browser, ok := browserRaw.(string)
+			if !ok {
+				// log.Println("cant cast browser to string")
+				continue
+			}
+			if ok := msieR.MatchString(browser); ok {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		// log.Println("Android and MSIE user:", user["name"], user["email"])
+		email := r.ReplaceAllString(user["email"].(string), " [at] ")
+		foundusersBuilder.WriteString(fmt.Sprintf("[%d] %s <%s>\n", userNumber, user["name"], email))
+
+	}
+	foundUsers := foundusersBuilder.String()
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
 }
