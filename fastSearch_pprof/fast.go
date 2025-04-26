9981537ae809098684da4e0c0b64443b208f81cb
pavspsp <sppavelsp000@gmail.com>
2025-04-26 16:33:47 +0300

    added easyjson

--- fastSearch_pprof/fast.go
+++ fastSearch_pprof/fast.go
@@ -2,14 +2,145 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
+	//"encoding/json"
+	json "encoding/json"
 	"fmt"
 	"io"
 	_ "io/ioutil"
 	"os"
 	"regexp"
 	"strings"
+
+	easyjson "github.com/mailru/easyjson"
+	jlexer "github.com/mailru/easyjson/jlexer"
+	jwriter "github.com/mailru/easyjson/jwriter"
 )
+
+type User struct {
+	Browsers []string `json:"browsers"`
+	Email    string   `json:"email"`
+	Name     string   `json:"name"`
+}
+
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+func easyjsonE4b0aaefDecodeHw3Data(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "email":
+			out.Email = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjsonE4b0aaefEncodeHw3Data(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"browsers\":"
+		out.RawString(prefix[1:])
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"email\":"
+		out.RawString(prefix)
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"name\":"
+		out.RawString(prefix)
+		out.String(string(in.Name))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjsonE4b0aaefEncodeHw3Data(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjsonE4b0aaefEncodeHw3Data(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjsonE4b0aaefDecodeHw3Data(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjsonE4b0aaefDecodeHw3Data(l, v)
+}
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
@@ -37,10 +168,10 @@ func FastSearch(out io.Writer) {
 	userNumber := -1
 	//users := make([]map[string]interface{}, 0)
 	for scanner.Scan() {
-		line := scanner.Text()
-		user := make(map[string]interface{})
+		var user User
+
 		// fmt.Printf("%v %v\n", err, line)
-		err := json.Unmarshal([]byte(line), &user)
+		err := user.UnmarshalJSON([]byte(scanner.Bytes()))
 		if err != nil {
 			panic(err)
 		}
@@ -50,18 +181,8 @@ func FastSearch(out io.Writer) {
 		isAndroid := false
 		isMSIE := false
 
-		browsers, ok := user["browsers"].([]interface{})
-		if !ok {
-			// log.Println("cant cast browsers")
-			continue
-		}
+		for _, browser := range user.Browsers {
 
-		for _, browserRaw := range browsers {
-			browser, ok := browserRaw.(string)
-			if !ok {
-				// log.Println("cant cast browser to string")
-				continue
-			}
 			if ok := androidR.MatchString(browser); ok {
 				isAndroid = true
 				notSeenBefore := true
@@ -76,14 +197,7 @@ func FastSearch(out io.Writer) {
 					uniqueBrowsers++
 				}
 			}
-		}
 
-		for _, browserRaw := range browsers {
-			browser, ok := browserRaw.(string)
-			if !ok {
-				// log.Println("cant cast browser to string")
-				continue
-			}
 			if ok := msieR.MatchString(browser); ok {
 				isMSIE = true
 				notSeenBefore := true
@@ -105,8 +219,8 @@ func FastSearch(out io.Writer) {
 		}
 
 		// log.Println("Android and MSIE user:", user["name"], user["email"])
-		email := r.ReplaceAllString(user["email"].(string), " [at] ")
-		foundusersBuilder.WriteString(fmt.Sprintf("[%d] %s <%s>\n", userNumber, user["name"], email))
+		email := r.ReplaceAllString(user.Email, " [at] ")
+		foundusersBuilder.WriteString(fmt.Sprintf("[%d] %s <%s>\n", userNumber, user.Name, email))
 
 	}
 	foundUsers := foundusersBuilder.String()
